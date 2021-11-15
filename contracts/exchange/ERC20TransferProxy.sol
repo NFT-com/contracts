9b45aec8da46794e51b76562836e8b0dea2a2982
Gavin Mai <gavinmai@alumni.stanford.edu>
2021-11-15 17:37:34 -0400

    edits

--- contracts/exchange/ERC20TransferProxy.sol
+++ contracts/exchange/ERC20TransferProxy.sol
@@ -1,13 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
 
-import "./roles/OperatorRole.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "./interfaces/IERC20TransferProxy.sol";
 
-contract ERC20TransferProxy is IERC20TransferProxy, Initializable, UUPSUpgradeable, OperatorRole {
+contract ERC20TransferProxy is IERC20TransferProxy, Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    mapping(address => bool) operators;
+
     function initialize() external initializer {
-        __Ownable_init();
         __UUPSUpgradeable_init();
+        __Context_init_unchained();
+        __Ownable_init_unchained();
     }
 
     function erc20safeTransferFrom(
@@ -17,6 +21,19 @@ contract ERC20TransferProxy is IERC20TransferProxy, Initializable, UUPSUpgradeab
         uint256 value
     ) external override onlyOperator {
         require(token.transferFrom(from, to, value), "failure while transferring");
+    }
+
+    function addOperator(address operator) external onlyOwner {
+        operators[operator] = true;
+    }
+
+    function removeOperator(address operator) external onlyOwner {
+        operators[operator] = false;
+    }
+
+    modifier onlyOperator() {
+        require(operators[_msgSender()], "OperatorRole: caller is not the operator");
+        _;
     }
 
     function _authorizeUpgrade(address) internal override onlyOwner {}
