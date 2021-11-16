97187ad2abe2479e6e14866ad05de92038dd917d
Gavin Mai <gavinmai@alumni.stanford.edu>
2021-11-16 12:02:19 -0400

    convert votes

--- contracts/NftToken.sol
+++ contracts/NftToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract NftToken is ERC20Permit {
+contract NftToken is ERC20Votes {
     constructor() ERC20Permit("NFT.com") ERC20("NFT.com", "NFT") {
         _mint(msg.sender, 10 * 10**9 * 10**18);
     }
@@ -15,4 +15,6 @@ contract NftToken is ERC20Permit {
     function burn(uint256 _amount) external {
         _burn(msg.sender, _amount);
     }
+
+
 }
