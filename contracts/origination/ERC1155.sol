0e315e60df71334dd7364326372b3d11de21f529
Gavin Mai <gavinmai@alumni.stanford.edu>
2021-11-16 17:54:27 -0400

    added placeholder contracts for lazy minting

---
+++ contracts/origination/ERC1155.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+
+// contract for minting custom 1155s on NFT.com
+// default contract for origination and lazy-mints
+contract NFT_COM_1155 {
+    constructor() {}
+}
