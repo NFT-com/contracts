60423128c735e21baaba080e2c11dcb1c7613cd6
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-02-03 18:12:30 -0400

    feat: add test

---
+++ test/nftOrigination.test.js
@@ -0,0 +1,21 @@
+const { expect } = require("chai");
+const { BigNumber } = require("ethers");
+
+// TODO: plan
+// setup marketplace environment
+// setup origination contract
+// setup bitpacking code locally to test address + index + maxSupply uint256
+// safeTransferFrom should mint when the supply < amount traded
+// upon `executeSwap`, the swap should originate a new NFT
+
+describe("Origination Testing", function () {
+  try {
+    beforeEach(async function () {});
+
+    describe("Origination Testing", async function () {
+      it("TBD", async function () {});
+    });
+  } catch (err) {
+    console.log("error: ", err);
+  }
+});
