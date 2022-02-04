3e407502e26c4e77ffd0592294f5a4c0bbfcced8
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-02-04 11:29:19 -0400

    feat: basic bitwise validation working

--- contracts/origination/TokenIdentifiers.sol
+++ contracts/origination/TokenIdentifiers.sol
@@ -5,19 +5,20 @@ pragma solidity >=0.8.4;
 // Clever library for bitpacking an address + index + supply into a uint256 to represent the tokenId for an ERC1155
 
 library TokenIdentifiers {
-    uint8 public constant ADDRESS_BITS = 160;
-    uint8 public constant INDEX_BITS = 56;
-    uint8 public constant SUPPLY_BITS = 40;
+    uint8 private constant ADDRESS_BITS = 160;
+    uint8 private constant INDEX_BITS = 56;
+    uint8 private constant SUPPLY_BITS = 40;
 
-    uint256 public constant SUPPLY_MASK = (uint256(1) << SUPPLY_BITS) - 1;
-    uint256 public constant INDEX_MASK = ((uint256(1) << INDEX_BITS) - 1) ^ SUPPLY_MASK;
+    uint256 private constant SUPPLY_MASK = (uint256(1) << SUPPLY_BITS) - 1;
+    uint256 private constant INDEX_MASK = ((uint256(1) << INDEX_BITS) - 1) ^ SUPPLY_MASK;
 
     function tokenMaxSupply(uint256 _id) internal pure returns (uint256) {
         return _id & SUPPLY_MASK;
     }
 
+    // modified from original code to shift bits right to remove supply bits
     function tokenIndex(uint256 _id) internal pure returns (uint256) {
-        return _id & INDEX_MASK;
+        return (_id & INDEX_MASK) >> SUPPLY_BITS;
     }
 
     function tokenCreator(uint256 _id) internal pure returns (address) {
