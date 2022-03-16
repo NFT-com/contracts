6c8b64ecb73463a4e41e699ddee5a9945270a3d2
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-03-16 13:19:41 -0400

    fix: update

--- tasks/deployers/index.ts
+++ tasks/deployers/index.ts
@@ -8,8 +8,8 @@ const wethAddress =
   network === "rinkeby" ? "0xc778417e063141139fce010982780140aa0cd5ab" : "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 const UNI_V2_FACTORY = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
 
-// STEP 1
-task("deploy:GenesisKey").setAction(async function (taskArguments, hre) {
+// STEP 1 (deploy:GenesisKey)
+task("deploy:1").setAction(async function (taskArguments, hre) {
   const name = "NFT.com Genesis Key";
   const symbol = "NFTKEY";
   const auctionSeconds = "604800"; // seconds in 1 week
@@ -24,8 +24,8 @@ task("deploy:GenesisKey").setAction(async function (taskArguments, hre) {
   console.log(chalk.green(`deployedGenesisKey: ${deployedGenesisKey.address}`));
 });
 
-// STEP 2
-task("deploy:NFT.com").setAction(async function (taskArguments, hre) {
+// STEP 2 deploy:NFT.com
+task("deploy:2").setAction(async function (taskArguments, hre) {
   console.log(chalk.green(`initializing...`));
   const deployedGenesisKeyAddress = "0xb5815c46D262005C170576330D0FB27d018fAd60"; // TODO: fill in after genesis key is done
 
@@ -91,8 +91,8 @@ task("deploy:NFT.com").setAction(async function (taskArguments, hre) {
   console.log(chalk.green(`deployedProfileAuction: ${deployedProfileAuction.address}`));
 });
 
-// Step 3
-task("deploy:NftMarketplace").setAction(async function (taskArguments, hre) {
+// Step 3 NftMarketplace
+task("deploy:3").setAction(async function (taskArguments, hre) {
   console.log(chalk.green("deploying the marketplace contacts..."));
 
   const rinkebyNFT = "0xa75F995f252ba5F7C17f834b314201271d32eC35";
@@ -102,6 +102,8 @@ task("deploy:NftMarketplace").setAction(async function (taskArguments, hre) {
   const NftTransferProxy = await hre.ethers.getContractFactory("NftTransferProxy");
   const ERC20TransferProxy = await hre.ethers.getContractFactory("ERC20TransferProxy");
   const CryptoKittyTransferProxy = await hre.ethers.getContractFactory("CryptoKittyTransferProxy");
+  const ValidationLogic = await hre.ethers.getContractFactory("ValidationLogic");
+  const MarketplaceEvent = await hre.ethers.getContractFactory("MarketplaceEvent");
 
   const deployedNftTransferProxy = await hre.upgrades.deployProxy(NftTransferProxy, { kind: "uups" });
   console.log(chalk.green("nftTransferProxy: ", deployedNftTransferProxy.address));
@@ -112,6 +114,12 @@ task("deploy:NftMarketplace").setAction(async function (taskArguments, hre) {
   const deployedCryptoKittyTransferProxy = await hre.upgrades.deployProxy(CryptoKittyTransferProxy, { kind: "uups" });
   console.log(chalk.green("deployedCryptoKittyTransferProxy: ", deployedCryptoKittyTransferProxy.address));
 
+  const deployedValidationLogic = await hre.upgrades.deployProxy(ValidationLogic, { kind: "uups" });
+  console.log(chalk.green("deployedValidationLogic: ", deployedValidationLogic.address));
+
+  const deployedMarketplaceEvent = await hre.upgrades.deployProxy(MarketplaceEvent, { kind: "uups" });
+  console.log(chalk.green("deployedMarketplaceEvent: ", deployedMarketplaceEvent.address));
+
   const deployedNftMarketplace = await hre.upgrades.deployProxy(
     NftMarketplace,
     [
@@ -120,11 +128,15 @@ task("deploy:NftMarketplace").setAction(async function (taskArguments, hre) {
       deployedCryptoKittyTransferProxy.address,
       deployedNftBuyer,
       rinkebyNFT,
+      deployedValidationLogic.address,
+      deployedMarketplaceEvent.address,
     ],
     { kind: "uups" },
   );
 
   console.log(chalk.green("deployedNftMarketplace: ", deployedNftMarketplace.address));
+
+  await deployedMarketplaceEvent.setMarketPlace(deployedNftMarketplace.address);
 
   // add operator being the marketplace
   await deployedNftTransferProxy.addOperator(deployedNftMarketplace.address);
@@ -134,8 +146,8 @@ task("deploy:NftMarketplace").setAction(async function (taskArguments, hre) {
   console.log(chalk.green("finished deploying nft marketplace contracts!"));
 });
 
-// STEP 4 (wait until ready)
-task("deploy:Airdrop").setAction(async function (taskArguments, hre) {
+// STEP 4 Airdrop (wait until ready)
+task("deploy:4").setAction(async function (taskArguments, hre) {
   // Profile Distributor Merkle ========================================================================
   // TODO: add in correct JSON mapping
   const jsonInput = JSON.parse(`{
@@ -161,7 +173,7 @@ task("upgrade:ProfileAuction").setAction(async function (taskArguments, { ethers
   await upgrades.upgradeProxy("0x7d4dDE9418f2c2d2D895C09e81155E1AB08aE236", ProfileAuction);
 });
 
-task("upgrade:NFTMarketplace").setAction(async function (taskArguments, hre) {
+task("upgrade:NftMarketplace").setAction(async function (taskArguments, hre) {
   console.log(chalk.green("starting to upgrade..."));
   const NftMarketplace = await hre.ethers.getContractFactory("NftMarketplace");
 
