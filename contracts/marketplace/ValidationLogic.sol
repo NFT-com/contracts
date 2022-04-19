0dda616260d8f440ba1c535906b6d40acd6d461d
Gavin <gmaijoe@users.noreply.github.com>
2022-04-19 11:01:53 -0400

    QSP Quality Fixes (#32)
    * Update TransferExecutor.sol
    
    * Update TransferExecutor.sol
    
    * Update ValidationLogic.sol
    
    * Update NftMarketplace.sol
    
    * Update GenesisNftStake.sol
    
    * Update NftMarketplace.sol
    
    * Update IMarketplaceEvent.sol
    
    * Update TransferExecutor.sol
    
    * Update ITransferExecutor.sol
    
    * Update TransferExecutor.sol
    
    * Update TransferExecutor.sol
    
    * Update ValidationLogic.sol
    
    * Update NftBuyer.sol
    
    * Update NftMarketplace.sol
    
    * Update ValidationLogic.sol
    
    * Update NftMarketplace.sol
    
    * Update NftMarketplace.sol
    
    * Update NftMarketplace.sol
    
    * Update GenesisNftStake.sol
    
    * Update NftBuyer.sol
    
    * Update NftMarketplace.sol
    
    * Update LpStake.sol
    
    * Update NftMarketplace.sol

--- contracts/marketplace/ValidationLogic.sol
+++ contracts/marketplace/ValidationLogic.sol
@@ -18,6 +18,7 @@ contract ValidationLogic is Initializable, UUPSUpgradeable, OwnableUpgradeable, 
      *  @dev validateSingleAssetMatch1 makes sure two assets can be matched (same index in LibSignature array)
      *  @param buyTakeAsset what the buyer is hoping to take
      *  @param sellMakeAsset what the seller is hoping to make
+     *  @return true if valid
      */
     function validateSingleAssetMatch1(LibAsset.Asset calldata buyTakeAsset, LibAsset.Asset calldata sellMakeAsset)
         internal
@@ -39,6 +40,7 @@ contract ValidationLogic is Initializable, UUPSUpgradeable, OwnableUpgradeable, 
      *  @param sellTakeAssetClass (bytes4 of type in LibAsset)
      *  @param buyMakeAssetTypeData assetTypeData for makeAsset on buyOrder
      *  @param sellTakeAssetTypeData assetTypeData for takeAsset on sellOrder
+     *  @return true if valid
      */
     function validAssetTypeData(
         bytes4 sellTakeAssetClass,
@@ -79,9 +81,10 @@ contract ValidationLogic is Initializable, UUPSUpgradeable, OwnableUpgradeable, 
     }
 
     /**
-     * @dev validateSingleAssetMatch2 makes sure two assets can be matched (same index in LibSignature array)
+     *  @dev validateSingleAssetMatch2 makes sure two assets can be matched (same index in LibSignature array)
      *  @param sellTakeAsset what the seller is hoping to take
      *  @param buyMakeAsset what the buyer is hoping to make
+     *  @return true if valid
      */
     function validateSingleAssetMatch2(LibAsset.Asset calldata sellTakeAsset, LibAsset.Asset calldata buyMakeAsset)
         internal
@@ -108,22 +111,22 @@ contract ValidationLogic is Initializable, UUPSUpgradeable, OwnableUpgradeable, 
     }
 
     /**
-     * @dev validateMatch makes sure two orders (on sell side and buy side) match correctly
-     * @param sellOrder the listing
-     * @param buyOrder bid for a listing
+     *  @dev validateMatch makes sure two orders (on sell side and buy side) match correctly
+     *  @param sellOrder the listing
+     *  @param buyOrder bid for a listing
+     *  @return true if orders can match
      */
     function validateMatch(
         LibSignature.Order calldata sellOrder,
-        LibSignature.Order calldata buyOrder,
-        bool viewOnly
+        LibSignature.Order calldata buyOrder
     ) internal view returns (bool) {
         // flag to ensure ETH is not used multiple timese
         bool ETH_ASSET_USED = false;
 
         require(
-            sellOrder.auctionType == LibSignature.AuctionType.English &&
-                buyOrder.auctionType == LibSignature.AuctionType.English,
-            "!english"
+            (sellOrder.auctionType == LibSignature.AuctionType.English) &&
+                (buyOrder.auctionType == LibSignature.AuctionType.English),
+            "vm auctionType"
         );
 
         // sellOrder taker must be valid
@@ -143,12 +146,6 @@ contract ValidationLogic is Initializable, UUPSUpgradeable, OwnableUpgradeable, 
         require(
             buyOrder.makeAssets.length != 0 && sellOrder.takeAssets.length == buyOrder.makeAssets.length,
             "vm assets > 0"
-        );
-
-        require(
-            (sellOrder.auctionType == LibSignature.AuctionType.English) &&
-                (buyOrder.auctionType == LibSignature.AuctionType.English),
-            "vm auctionType"
         );
 
         // check if seller maker and buyer take match on every corresponding index
@@ -197,9 +194,10 @@ contract ValidationLogic is Initializable, UUPSUpgradeable, OwnableUpgradeable, 
     }
 
     /**
-     * @dev validateBuyNow makes sure a buyer can fulfill the sellOrder and that the sellOrder is formatted properly
-     * @param sellOrder the listing
-     * @param buyer potential executor of sellOrder
+     *  @dev validateBuyNow makes sure a buyer can fulfill the sellOrder and that the sellOrder is formatted properly
+     *  @param sellOrder the listing
+     *  @param buyer potential executor of sellOrder
+     *  @return true if validBuyNow
      */
     function validateBuyNow(LibSignature.Order calldata sellOrder, address buyer) public view override returns (bool) {
         require((sellOrder.taker == address(0) || sellOrder.taker == buyer), "vbn !match");
@@ -217,9 +215,10 @@ contract ValidationLogic is Initializable, UUPSUpgradeable, OwnableUpgradeable, 
     }
 
     /**
-     * @dev public facing function to make sure orders can execute
-     * @param sellOrder the listing
-     * @param buyOrder bid for a listing
+     *  @dev public facing function to make sure orders can execute
+     *  @param sellOrder the listing
+     *  @param buyOrder bid for a listing
+     *  @return true if valid match
      */
     function validateMatch_(LibSignature.Order calldata sellOrder, LibSignature.Order calldata buyOrder)
         public
@@ -227,9 +226,14 @@ contract ValidationLogic is Initializable, UUPSUpgradeable, OwnableUpgradeable, 
         override
         returns (bool)
     {
-        return validateMatch(sellOrder, buyOrder, true);
+        return validateMatch(sellOrder, buyOrder);
     }
-
+    
+    /**
+     *  @dev public facing function to get current price of a decreasing price auction
+     *  @param sellOrder the listing
+     *  @return current price denominated in the asset specified
+     */
     function getDecreasingPrice(LibSignature.Order calldata sellOrder) public view override returns (uint256) {
         require(sellOrder.auctionType == LibSignature.AuctionType.Decreasing, "gdp !decreasing");
         decreasingValidation(sellOrder);
