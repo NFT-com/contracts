b20453c4e53869199942613937cfe0bcd0da01d2
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-08-05 12:55:55 +0200

    fix: use rinkeby

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -102,7 +102,7 @@ const config: HardhatUserConfig = {
     mainnet: getChainConfigPK("mainnet"),
     goerli: getChainConfig("goerli"),
     kovan: getChainConfig("kovan"),
-    rinkeby: getChainConfig("rinkeby"), //getChainConfigPK("rinkeby"),
+    rinkeby: getChainConfigPK("rinkeby"), //getChainConfigPK("rinkeby"),
     ropsten: getChainConfig("ropsten"),
   },
   contractSizer: {
