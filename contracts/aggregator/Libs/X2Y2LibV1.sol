cbd2d47b9260fb3dde906e40b1bf2da9c3bc64e4
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-10-04 13:50:37 -0400

    fix: contract work

--- contracts/aggregator/Libs/X2Y2LibV1.sol
+++ contracts/aggregator/Libs/X2Y2LibV1.sol
@@ -1,9 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
 
-interface IX2Y2 {}
+import "./X2Y2/structs.sol";
 
-/// @title TODO
+interface IX2Y2 {
+    function run(
+        RunInput memory input
+    ) external;
+}
+
 library X2Y2LibV1 {
     address public constant X2Y2 = 0x74312363e45DCaBA76c59ec49a7Aa8A65a67EeD3;
+
+    function _run(
+        RunInput memory _input,
+        uint256 _msgValue,
+        bool _revertIfTrxFails
+    ) internal {
+        bytes memory _data = abi.encodeWithSelector(
+            IX2Y2.run.selector,
+            _input
+        );
+
+        (bool success, ) = X2Y2.call{ value: _msgValue }(_data);
+
+        if (!success && _revertIfTrxFails) {
+            // Copy revert reason from call
+            assembly {
+                returndatacopy(0, 0, returndatasize())
+                revert(0, returndatasize())
+            }
+        }
+    }
 }
--- test/utils/aggregator/xy2yHelper.ts
+++ test/utils/aggregator/xy2yHelper.ts
@@ -134,68 +134,6 @@ export async function signOrderForX2Y2(
   await signOrder(signer, order)
 }
 
-const input = {
-  'input': {
-    'orders': [
-      {
-        'salt': '330301253372159475838719893497397244662',
-        'user': '0x3beff89067d985a08fca612cfb3ccd4986314ed9',
-        'network': '1',
-        'intent': '1',
-        'delegateType': '1',
-        'deadline': '1671914511',
-        'currency': '0x0000000000000000000000000000000000000000',
-        'dataMask': '0x',
-        'items': [
-          {
-            'price': '15000000000000000',
-            'data': '0x000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000406243ca557c259712be750773177bc714e49f6000000000000000000000000000000000000000000000000000000000000155d'
-          }
-        ],
-        'r': '0xfded63691a42b760ceef8510da27bee9f19353c5428e5bb7e9c36881383885d0',
-        's': '0x76391d86ae9744ba458d13704b8bc735dbe28eff95cd4627fc06647dc359be50',
-        'v': 27,
-        'signVersion': 1
-      }
-    ],
-    'details': [
-      {
-        'op': 1,
-        'orderIdx': '0',
-        'itemIdx': '0',
-        'price': '15000000000000000',
-        'itemHash': '0xd951cf61c8a89d239fb31876175695123ea9a798481a633a32173b4a287bea18',
-        'executionDelegate': '0xf849de01b080adc3a814fabe1e2087475cf2e354',
-        'dataReplacement': '0x',
-        'bidIncentivePct': '0',
-        'aucMinIncrementPct': '0',
-        'aucIncDurationSecs': '0',
-        'fees': [
-          {
-            'percentage': '5000',
-            'to': '0xd823c605807cc5e6bd6fc0d7e4eea50d3e2d66cd'
-          },
-          {
-            'percentage': '100000',
-            'to': '0x80cfd0322554eb3cb263fd29559828440f957582'
-          }
-        ]
-      }
-    ],
-    'shared': {
-      'salt': '243807992699659',
-      'deadline': '1664905515',
-      'amountToEth': '0',
-      'amountToWeth': '0',
-      'user': '0x487f09bd7554e66f131e24edc1efee0e0dfa7fd1',
-      'canFail': false
-    },
-    'r': '0x57f844abfdb3f3e8172ed61e2fa593f11b359902709f3334eea97ab85d921a26',
-    's': '0x5f18ada7732af6e8fa13074260f92c4c3ac62f09ea0b1748a73a05da88586475',
-    'v': 28
-  }
-}
-
 export async function createX2Y2ParametersForNFTListing(
   network: Network,
   signer: ethers.Signer,
